521e162b0b5195b8e8e8561274dab4aeff5a059c
Ronando <ronando.long@gmail.com>
2016-02-01 11:05:08 -0800

    Spec passes: Constructor passes, it will create an object called survivor

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
-// Business Logic
-// var helloWorld = function(){
-//   return false;
-//
-//   For loops and shit go here
-//   If/else like a madman here
-//
-//  return shit like a mo-fo here
-//
-//   ...whatchyu lookin at bitch? Green those specs and git back to work!
-//  Time fo yo Interface Logic, then suckface when it's all green.
-// };
+function Survivor (name, phone, note, street, city, state, zip) {
+  this.name = name;
+  this.phone = phone;
+  this.note = note;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+}
 
 
 
@@ -21,17 +18,7 @@
 // User Interface Logic
 // $(document).ready(function() {
 //   $("form#IDselector").submit(function(event) {
-//     // variables and shit go here, bitch!
 //
-//     // link your muthafuckin' variables to yo bad-ass bidness logic function, Yo!
-//
-//     // Make shit happen here...BITCH!
-//
-//     // Muthafuckin' variables connect to your output down here hommie.
-//
-//     // Show me some money, hunny, and reveal that hiddin fine shit you got stashed.
-//
-//     // It's all good baby.'
 //     event.preventDefault();
 //   });
 // });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+describe('Survivor', function(){
+  it("will create an object called survivor", function(){
+    var testSurvivor = new Survivor("Bob", 1234567890, "hello world", "400 SW 6th Ave", "Portland", "Oregon", 97204)
+    expect(testSurvivor.name).to.equal("Bob");
+    expect(testSurvivor.phone).to.equal(1234567890);
+    expect(testSurvivor.note).to.equal("hello world");
+    expect(testSurvivor.street).to.equal("400 SW 6th Ave");
+    expect(testSurvivor.city).to.equal("Portland");
+    expect(testSurvivor.state).to.equal("Oregon");
+    expect(testSurvivor.zip).to.equal(97204);
+  });
+
+});
