319fbb82aa545e37cb93a9b0f15f5eed9877550e
Ronando <ronando.long@gmail.com>
2016-02-01 15:28:29 -0800

    Pass first and second spec

--- spec/specs.js
+++ spec/specs.js
@@ -10,7 +10,8 @@ describe('Survivor', function(){
     expect(testSurvivor.zip).to.equal(97204);
   });
 
-  it("will obtain the survivor's location via geolocation", function(){
+
+  it("will obtain the survivor's location via form input", function(){
     var testSurvivor = new Survivor("Bob", 1234567890, "hello world", "400 SW 6th Ave", "Portland", "Oregon", 97204)
     expect(testSurvivor.address()).to.equal("400 SW 6th Ave, Portland, Oregon 97204");
   });
